061e03cbd5f02100950f86105ece06789110fc9b
Janos SUTO <sj@acts.hu>
2023-02-07 20:23:43 +0100

    Added read_products()
    Signed-off-by: Janos SUTO <sj@acts.hu>

--- receipt.py
+++ receipt.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+import sys
 
 
 IMPORT_TAX = 0.05
@@ -25,8 +26,27 @@ def calc_basic_sales_tax(product, price):
     return price * SALES_TAX
 
 
+def read_products():
+    """
+    Read product info from stdin, 1 product per line.
+    Fields are separated by semicolon (;), eg.
+    1;book;14.90
+
+    We also assume that each line is valid, and
+    has the required fields with appropriate values.
+    """
+    products = []
+
+    for line in sys.stdin:
+        (amount, product, price) = line.rstrip().split(';')
+        products.append((int(amount), product, float(price)))
+
+    return products
+
+
 def main():
-    pass
+    for (amount, product, price) in read_products():
+        print(amount, product, price)
 
 
 if __name__ == '__main__':
